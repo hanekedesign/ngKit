b7afffff1ffbd83a1a24881092babdcd1a436aa1
Mark Smith <msmith46234@yahoo.com>
2018-08-24 08:57:19 -0400

    Revert "Feature/auth registration"

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { Config } from './config';
-export { Authentication, Authorization, Cache, Event, Http, SocialAuthentication, Storage, Token } from './services/index';
-export { AuthGuard, AuthResolveGuard } from './guards/index';
-export { NGKIT_PROVIDERS } from './providers';
-export { Model } from './models';
-export { ngKitModule } from './ngkit.module';
--- dist/ngkit.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './index';
-export { AuthInterceptor as ɵb } from './services/http-auth-interceptor';
-export { HttpInterceptor as ɵa } from './services/http-interceptor';
